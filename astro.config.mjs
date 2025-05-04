27430fbdae31c05b8a43b5c7ded3890a2bbd57b2
Martin Alexander Ramos Yampufé <alexanderamsyam@gmail.com>
2025-05-04 09:02:19 -0500

    fix: Ensure trailing slash is always present in site configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: "https://har.vluepixel.com",
+  trailingSlash: "always",
 
   integrations: [react()],
 
