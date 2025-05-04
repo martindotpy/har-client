6ad1ea59255d79f42f2915438a2677ddd906e8b9
Martin Alexander Ramos Yampufé <alexanderamsyam@gmail.com>
2025-05-04 18:22:18 -0500

    feat: Add internationalization support and override texts for copy buttons in Spanish

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,23 @@
 // @ts-check
 import react from "@astrojs/react";
+import { pluginFramesTexts } from "@expressive-code/plugin-frames";
 import tailwindcss from "@tailwindcss/vite";
 import expressiveCode from "astro-expressive-code";
 import { defineConfig } from "astro/config";
 
+pluginFramesTexts.overrideTexts("es", {
+  copyButtonTooltip: "Copiar",
+  copyButtonCopied: "¡Copiado!",
+});
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://har.vluepixel.com",
+
+  i18n: {
+    defaultLocale: "es",
+    locales: ["es"],
+  },
 
   integrations: [
     react(),
@@ -16,6 +27,7 @@ export default defineConfig({
         uiFontFamily: "var(--font-sans)",
         codeFontFamily: "var(--font-mono)",
       },
+      defaultLocale: "es",
     }),
   ],
 
